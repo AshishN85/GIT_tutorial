ad6267c526459ee814bb1b23ad4249503aaf4e11
Ashish Naidu <ashish.naidu1985@gmail.com>
2025-01-22 08:32:02 +0530

    my snapshot

--- demo1.js
+++ demo1.js
@@ -1,1 +1,6 @@
 console.log(`Ashish Naidu's + ' ' + 'GIT & GitHub' + ' ' + 'Demo`);
+
+//Hello everyone, I'm Ashish and I always keen on learning something new in the Techie world!! What fun it is!!
+
+console.log(`Hello everyone, I'm Ashish and I always keen on learning something new in the Techie world!! 
+    What fun it is!!`);
